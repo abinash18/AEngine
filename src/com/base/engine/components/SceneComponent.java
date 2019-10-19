b95ded23fdf30c7171406c424c32b5bea06a0777
BOOZIE <abinashbhullar.as@gmail.com>
2019-10-19 11:36:48 -0600

    Fixed Some Files.
    Former-commit-id: d924b96edf179c1ebe652423b3ab38177fc8c4df

--- src/com/base/engine/components/SceneComponent.java
+++ src/com/base/engine/components/SceneComponent.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/com/base/engine/components/SceneComponent.java
 package com.base.engine.components;
 
 import com.base.engine.core.GameObject;
@@ -53,59 +52,3 @@ public abstract class SceneComponent {
 	}
 
 }
-=======
-package com.base.engine.components;
-
-import com.base.engine.core.GameObject;
-import com.base.engine.math.Transform;
-import com.base.engine.rendering.RenderingEngine;
-import com.base.engine.rendering.sceneManagement.Scene;
-import com.base.engine.rendering.shaders.Shader;
-
-public abstract class SceneComponent {
-
-	private GameObject parent;
-	private String Name;
-
-	public Transform getTransform() {
-		return parent.getTransform();
-	}
-
-	public Scene getParentScene() {
-		return parent.getParentScene();
-	}
-
-	public GameObject getParent() {
-		return parent;
-	}
-
-	public void setParent(GameObject parent) {
-		this.parent = parent;
-	}
-
-	public void init() {
-	}
-
-	public void update(float delta) {
-	}
-
-	public void input(float delta) {
-	}
-
-	public void render(Shader shader, RenderingEngine engine) {
-	}
-
-	public void addToScene() {
-
-	}
-
-	public String getName() {
-		return Name;
-	}
-
-	public void setName(String name) {
-		Name = name;
-	}
-
-}
->>>>>>> s:src/com/base/engine/components/SceneComponent.java
